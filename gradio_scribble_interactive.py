52183537738d4608c2a68a1c00eaf98622571b38
darrellsilver <24387+darrellsilver@users.noreply.github.com>
2023-04-25 14:32:08 -0400

    .

--- gradio_scribble_interactive.py
+++ gradio_scribble_interactive.py
@@ -73,26 +73,24 @@ def create_canvas(w, h):
 
 block = gr.Blocks().queue()
 with block:
-    with gr.Row():
-        gr.Markdown("## Control Stable Diffusion with Interactive Scribbles")
-    with gr.Row():
-        with gr.Column():
-            input_image = gr.Sketchpad(tool='sketch', shape=(1024, 1024))
-            prompt = gr.Textbox(label="Style")
-            run_button = gr.Button(label="Wave")
-            with gr.Accordion("Advanced options", open=False):
-                num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
-                image_resolution = gr.Slider(label="Image Resolution", minimum=256, maximum=768, value=512, step=64)
-                strength = gr.Slider(label="Control Strength", minimum=0.0, maximum=2.0, value=1.0, step=0.01)
-                guess_mode = gr.Checkbox(label='Guess Mode', value=False)
-                ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=10, step=1)
-                scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=9.0, step=0.1)
-                eta = gr.Slider(label="DDIM ETA", minimum=0.0, maximum=1.0, value=1.0, step=0.01)
-                a_prompt = gr.Textbox(label="Added Prompt", value='best quality')
-                n_prompt = gr.Textbox(label="Negative Prompt", value='lowres, bad anatomy, bad hands, cropped, worst quality')
-                seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, value=12345)
+    with gr.Column():
+        input_image = gr.Sketchpad(tool='sketch', shape=(1024, 1024))
+        prompt = gr.Textbox(label="Style")
+        run_button = gr.Button(value="Wave")
     with gr.Column():
         result_gallery = gr.Gallery(label='Output', show_label=False, elem_id="gallery").style(grid=2, height='auto')
+    with gr.Row():
+        with gr.Accordion("Advanced options", open=False):
+            num_samples = gr.Slider(label="Images", minimum=1, maximum=12, value=1, step=1)
+            image_resolution = gr.Slider(label="Image Resolution", minimum=256, maximum=768, value=512, step=64)
+            strength = gr.Slider(label="Control Strength", minimum=0.0, maximum=2.0, value=1.0, step=0.01)
+            guess_mode = gr.Checkbox(label='Guess Mode', value=False)
+            ddim_steps = gr.Slider(label="Steps", minimum=1, maximum=100, value=10, step=1)
+            scale = gr.Slider(label="Guidance Scale", minimum=0.1, maximum=30.0, value=9.0, step=0.1)
+            eta = gr.Slider(label="DDIM ETA", minimum=0.0, maximum=1.0, value=1.0, step=0.01)
+            a_prompt = gr.Textbox(label="Added Prompt", value='best quality')
+            n_prompt = gr.Textbox(label="Negative Prompt", value='lowres, bad anatomy, bad hands, cropped, worst quality')
+            seed = gr.Slider(label="Seed", minimum=-1, maximum=2147483647, step=1, value=12345)
     ips = [input_image, prompt, a_prompt, n_prompt, num_samples, image_resolution, ddim_steps, guess_mode, strength, scale, seed, eta]
     run_button.click(fn=process, inputs=ips, outputs=[result_gallery])
 
